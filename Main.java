eb8ccf3f801669afa9728bcc6b078d7ffafa657b
zolangi <zolangiramirez@gmail.com>
2017-05-10 17:14:58 -0700

    Update KeyGen.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package project2;
+
+/**
+ * @author Zolangi Ramirez
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
